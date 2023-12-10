4a35bc2e12a747ad799f68262d0f237a8da2f63a
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-10 21:00:51 +0530

    youtube video popup added

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,6 +1,5 @@
-
-        // popup video
-        // Function to open the video popup
+// popup for youtube
+// Function to open the video popup
         function openVideoPopup() {
             var videoPopup = document.getElementById('videoPopup');
             videoPopup.style.display = 'block';
@@ -16,3 +15,24 @@
         window.onload = function () {
             openVideoPopup();
         };
+
+
+
+
+// popup for video from directory
+        // // Function to open the video popup
+        // function openVideoPopup() {
+        //     var videoPopup = document.getElementById('videoPopup');
+        //     videoPopup.style.display = 'block';
+        // }
+
+        // // Function to close the video popup
+        // function closeVideoPopup() {
+        //     var videoPopup = document.getElementById('videoPopup');
+        //     videoPopup.style.display = 'none';
+        // }
+
+        // // Open the video popup when the page loads
+        // window.onload = function () {
+        //     openVideoPopup();
+        // };
