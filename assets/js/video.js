2c3ea3ff62b9ddab203bb3bb29096da7065c9de2
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-10 18:40:10 +0530

    Event page added

---
+++ assets/js/video.js
@@ -0,0 +1,18 @@
+
+        // popup video
+        // Function to open the video popup
+        function openVideoPopup() {
+            var videoPopup = document.getElementById('videoPopup');
+            videoPopup.style.display = 'block';
+        }
+
+        // Function to close the video popup
+        function closeVideoPopup() {
+            var videoPopup = document.getElementById('videoPopup');
+            videoPopup.style.display = 'none';
+        }
+
+        // Open the video popup when the page loads
+        window.onload = function () {
+            openVideoPopup();
+        };
